db609d22ee3cb602d19bd3b89479d0262c3e73c5
YukiTsuyama <60831865+YukiTsuyama@users.noreply.github.com>
2020-06-16 08:02:33 +0900

    Update WorkPlaceCommunication.java

--- src/main/WorkPlaceCommunication.java
+++ src/main/WorkPlaceCommunication.java
@@ -21,6 +21,14 @@ public class WorkPlaceCommunication implements Communication{
 		}
 	}
 
+// 　　　11職場じゃなければ挨拶しない（修正版）
+	public void judgement(int place) {
+		if(place == 0) {
+			System.out.println(name + "さん" + greeting);
+		} else if(place == 1){
+			System.out.println("..............");
+		}
+	}
 
 
 }
