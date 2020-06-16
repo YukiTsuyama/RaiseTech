3a806297ae1a1bc0c356d73f843cb3c22280c219
YukiTsuyama <60831865+YukiTsuyama@users.noreply.github.com>
2020-06-16 18:45:22 +0900

    Update WorkPlaceCommunication.java

--- src/main/WorkPlaceCommunication.java
+++ src/main/WorkPlaceCommunication.java
@@ -5,27 +5,42 @@ package main;
 public class WorkPlaceCommunication implements Communication{
 
 //	⑩職場か判断するフィールドを作ってみよう
-	boolean workstation;
+// 	boolean workstation;
+	
 
 
 	String greeting = "Good Morning";
 	String name = "Yuki";
 
 
-//	11職場じゃなければ挨拶しない
-	public void judgement() {
-		if(workstation = true) {
-			System.out.println(name + "さん" + greeting);
-		} else {
-			System.out.println("..............");
-		}
-	}
+// //	11職場じゃなければ挨拶しない
+// 	public void judgement() {
+// 		if(workstation = true) {
+// 			System.out.println(name + "さん" + greeting);
+// 		} else {
+// 			System.out.println("..............");
+// 		}
+// 	}
 
-// 　　　11職場じゃなければ挨拶しない（修正版）
-	public void judgement(int place) {
-		if(place == 0) {
+	
+	
+// // 　　　11職場じゃなければ挨拶しない（修正版1）
+// 	public void judgement(int place) {
+// 		if(place == 0) {
+// 			System.out.println(name + "さん" + greeting);
+// 		} else if(place == 1){
+// 			System.out.println("..............");
+// 		}
+// 	}
+	
+	
+	
+// 	11職場じゃなければ挨拶しない（修正版2）
+	boolean workStation = true;
+	public void judgement(boolean workStation) {
+		if(workStation) {
 			System.out.println(name + "さん" + greeting);
-		} else if(place == 1){
+		} else{
 			System.out.println("..............");
 		}
 	}
